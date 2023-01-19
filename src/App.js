c54f9f186576e156f1e5a80755e88dbf2aaea256
KenanAljaber <keno12333@hotmail.com>
2023-01-19 01:48:53 -0300

    the simple implementaion of converting image to base64 and vice versa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import ConvertImageToBase64 from "./components/ConvertImageToBase64";
+import "./index.css";
+
+
 
 function App() {
+  const [encodedImage, setEncodedImage] = useState(null);
+  const [copyToClipboardTxt, setCopyToClipboardTxt] = useState("Click here to copy");
+  const copyBtn = useRef(null);
+
+  useEffect(() => {
+
+  }, [encodedImage]);
+  function handleChange(e) {
+    ConvertImageToBase64(e.target.files[0], onResult);
+  }
+
+  //the callback to get the result of the encodded image
+  function onResult(result) {
+    setEncodedImage(result);
+    setCopyToClipboardTxt("Click here to copy!");
+    copyBtn.current.disabled = false;
+  }
+
+  function copyToClipBoard(e) {
+    navigator.clipboard.writeText(encodedImage.base64);
+    setCopyToClipboardTxt("Copied!");
+    copyBtn.current.disabled = true;
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{ textAlign: "center" }}>
+
+      <input type='file' id='file' onChange={handleChange} />
+      {encodedImage && <div> <h2>this is the encoded image after decoded it</h2>
+      {/*this is the image after decoded it*/ }
+        <img width="750px" style={{ "borderRadius": "10px", "boxShadow": "5px 7px 19px 0px #4ddca0" }}
+          src={`data:image/jpeg;base64,${encodedImage.base64}`} />
+      </div>}
+      {encodedImage &&
+        <div>
+          <h2>this is the Base64 of the uploaded image</h2>
+          <button ref={copyBtn} onClick={copyToClipBoard}>{copyToClipboardTxt}</button>
+          <p style={{ overflowWrap: "break-word" }}>{encodedImage ? encodedImage.base64 : ""}</p>
+        </div>}</div>
   );
 }
 
